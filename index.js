172acbdf295737fd431fe44b24f7bbaffad8478e
ulugbek1997 <ulugbekxolmirzayev97@gmail.com>
2023-04-11 14:34:53 +0500

    express haqida tushuncha

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ const express = require("express")
 
 const app = express()
 
+const  users = [
+    {id:1,username:"fifa"},
+    {id:2,username:"fish"}
+
+]
+
 app.get("/",(req,res)=>{
     res.json({
         "message":"Hello World"
     })
 })
-
+app.get("/users",(req,res)=>{
+    res.json(users)
+})
+app.get("/users/:id",(req,res)=>{
+    const user = users.find(u=>u.id==req.params.id)
+    res.json(user)
+})
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
 })
