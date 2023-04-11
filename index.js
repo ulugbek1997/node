ea05e4def86e45bc61ae70853b0bc8ecd92f0254
ulugbek1997 <ulugbekxolmirzayev97@gmail.com>
2023-04-11 12:57:51 +0500

    expsess install

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("ok")
+const express = require("express")
+
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.json({
+        "message":"Hello World"
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000")
+})
